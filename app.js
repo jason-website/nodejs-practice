490d7db56755a66f112edbb80253ea4e93cc47c0
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-05 17:22:07 +0800

    [5.63] feat: handing different routes

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 //The function inside it will be executed for every incoming request.
 //Can not rename these three arguments.
 //Next is a function will be executed to allow the request to travel on to the next mildWare.
-app.use((req, res, next)=>{
-    console.log('In the middleWare');
-    next();
+app.use('/add-product',(req, res, next)=>{
+    console.log('In the another middleWare');
+    res.send('<h1>The "Add Product" Page</h1>')//allow us to attach a body, default header is text/html
 });
-app.use((req, res, next)=>{
+app.use('/',(req, res, next)=>{
     console.log('In the another middleWare');
     res.send('<h1>Hello From Express!</h1>')//allow us to attach a body, default header is text/html
 });
