064e9a19fc6ef5a3e5d7b63123bf1d7fd8b68494
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-05 14:34:39 +0800

    [3.39] refactor: extract routes module from app

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });//I want to listen for the data event. the data event will be fired whenever a new chunk is ready to be read.
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();//buffer is our bus stop. I know the incoming data will be text.
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, error => {
-            res.statusCode = 302;
-            res.setHeader('Location', '/');//location is the default header accepted by the browser. Using slash means it
-            //will use the host we are already running on.
-            return res.end();
-            });
-        });//this will be fired once it is done parsing the incoming requests data or incoming requests general
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+
+const server = http.createServer(routes);
 
 server.listen(3000);
