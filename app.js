7705a955005a7a24b7aa4bcc1aecc4645417f7f1
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-06 14:47:13 +0800

    [5.75] refactor: extract css code and make access to it

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const path = require('path');
 const app = express();
 // it registers a middleware, this function in the end just yields us such a middleware function.
 app.use(bodyParser.urlencoded({extended: false}))//should be able to parse non-default features
+
+app.use(express.static(path.join(__dirname,'public')))//it serveres static files. we can execute this function. we just have to pass in a path to the folder
+// which we want to serve statically. so basically a folder which we want to grant access to
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
