629b1a0942f1bcc90c26dda66f45f322b5a697ef
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-06 13:52:26 +0800

    [5.70] feat: use path to send html file to user

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,10 @@ const express = require('express');
 //This router is like a mini express app tied to the other express app or pluggable into the other express app
 const router = express.Router();
 
+const path = require('path');
+
 router.get('/add-product',(req, res, next)=>{
-    console.log('In the another middleWare');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')//allow us to attach a body, default header is text/html
+res.sendFile(path.join(__dirname,'../','views','add-product.html'))
 });
 router.post('/add-product',(req, res, next)=>{
     console.log(req.body);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello From Express!</h1>')//allow us to attach a body, default header is text/html
+    //__dirname this is global variable which simply holds the absolute path  on our operating system.
+    //path.join() 会自动识别是哪个系统然后自动生成对应的路径
+    //../go up one level
+res.sendFile(path.join(__dirname,'../','views','shop.html'))//send file to the user
 });
 
 module.exports = router;
