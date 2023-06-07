782e70c30491c916423cc0ad99dbe40d91c8ff04
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-07 17:20:42 +0800

    [9.123] feat: add Cart mode to add a product into a cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@
 const adminData = require("../routes/admin");
 
 const Product = require('../models/product');
+const Cart = require('../models/carts');
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop/product-list', {
@@ -38,6 +39,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const productId = req.body.productId;
+    Product.findById(productId,product=>{
+        Cart.addProduct(productId, product.price);
+    })
     console.log(productId);
     res.redirect('/cart');
 };
