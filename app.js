e9022954db3319866d0b4b0aadd4f6050944c0bb
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-07 10:26:16 +0800

    [7.99] fix: pass path variable to 404 ejs

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.set('views', 'views');
 // it registers a middleware, this function in the end just yields us such a middleware function.
 app.use(bodyParser.urlencoded({extended: false}))//should be able to parse non-default features
 
-app.use(express.static(path.join(__dirname,'public')))//it serveres static files. we can execute this function. we just have to pass in a path to the folder
+app.use(express.static(path.join(__dirname, 'public')))//it serveres static files. we can execute this function. we just have to pass in a path to the folder
 // which we want to serve statically. so basically a folder which we want to grant access to
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(404).render('404',{pageTitle: 'Page Not Found'});
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path: '/'});
 })
 
 app.listen(3000);
