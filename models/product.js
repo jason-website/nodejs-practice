c52354ac179f3576d1b9508cf861650c71573119
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-07 14:54:32 +0800

    [9.118] feat: add id to product

--- models/product.js
+++ models/product.js
@@ -22,6 +22,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFIle(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
