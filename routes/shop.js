7af094581f1f4e25641a3addfe972b47a86c3144
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-06-07 13:37:05 +0800

    [8.109] feat: register the routes with new views

--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const shopController = require('../controllers/shop');
 
-router.get('/', shopController.getProducts);
+router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
-router.get('/cart', shopController.getProducts);
-router.get('/checkout', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
